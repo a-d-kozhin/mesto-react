56bc4a6ccf78b8da329faaa69b4db7a0372daa91
Alexander Kozhin <a.d.kozhin@yandex.ru>
2020-08-16 16:54:35 +0300

    fix: popup issue

--- src/components/App.js
+++ src/components/App.js
@@ -9,28 +9,25 @@ function App() {
   const [isEditProfilePopupOpen, setEditProfilePopupOpen] = React.useState(false);
   const [isEditAvatarPopupOpen, setEditAvatarPopupOpen] = React.useState(false);
   const [isAddPlacePopupOpen, setAddPlacePopupOpen] = React.useState(false);
-  const [isPopupClosed, setPopupClosed] = React.useState(true);
 
   function handleEditAvatarClick() {
     setEditAvatarPopupOpen(true);
-    setPopupClosed(false);
   }
 
   function handleEditProfileClick() {
     setEditProfilePopupOpen(true);
-    setPopupClosed(false);
   }
 
   function handleAddPlaceClick() {
     setAddPlacePopupOpen(true);
-    setPopupClosed(false);
   }
 
   function closeAllPopups() {
     document.querySelector('.popup_opened').classList.remove('popup_opened');
-    setPopupClosed(true);
+    setEditAvatarPopupOpen(false);
+    setEditProfilePopupOpen(false);
+    setAddPlacePopupOpen(false);
   }
-
 
   return (
     <body className="project">
